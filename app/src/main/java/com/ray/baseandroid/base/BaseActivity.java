c4f86dfd6ee6f0b5d642862178cd92231e787883
leixing <leixing1012@gmail.com>
2017-04-17 17:51:55 +0800

    #添加DialogActivity测试及相关资源文件

---
+++ app/src/main/java/com/ray/baseandroid/base/BaseActivity.java
@@ -0,0 +1,44 @@
+package com.ray.baseandroid.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Author      : leixing
+ * Date        : 2017-04-14
+ * Email       : leixing@hecom.cn
+ * Version     : 0.0.1
+ * <p>
+ * Description : xxx
+ */
+
+public abstract class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initVariables();
+        initView();
+        loadData();
+    }
+
+    protected abstract void initVariables();
+
+    protected abstract void initView();
+
+    protected abstract void loadData();
+
+    protected <T extends View> T findView(int id) {
+        View view = findViewById(id);
+        if (view == null) {
+            throw new IllegalArgumentException("can not find view(id:" + id + ") in activity:" + this.getClass().getSimpleName());
+        }
+        return (T) view;
+    }
+
+    protected void showToast(CharSequence text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+}
