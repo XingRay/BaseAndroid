70297262bf697039149c9dd369e5e62a27f8617b
leixing <leixing1012@gmail.com>
2017-05-05 10:28:39 +0800

    #AppUtil添加判断应用是否安装的方法

--- app/src/main/java/com/ray/baseandroid/util/AppUtil.java
+++ app/src/main/java/com/ray/baseandroid/util/AppUtil.java
@@ -2,6 +2,8 @@ package com.ray.baseandroid.util;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.text.TextUtils;
 
 import java.util.List;
@@ -23,8 +25,8 @@ public class AppUtil {
     /**
      * APP是否正处于前台运行
      *
-     * @param context
-     * @return
+     * @param context 上下文
+     * @return 应用是否在前台
      */
     public static boolean isAppOnForeground(Context context) {
         if (context == null) {
@@ -56,5 +58,23 @@ public class AppUtil {
             }
         }
         return false;
+    }
+
+    /**
+     * 根据包名判断app是否安装了
+     *
+     * @param context     上下文
+     * @param packageName 应用的包名
+     * @return 该应用是否安装
+     */
+    public static boolean isAppInstalled(Context context, String packageName) {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = context.getPackageManager().getPackageInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return packageInfo != null;
     }
 }
