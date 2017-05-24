beda0215fe69ff5a33ea816e0df6a36e508e1dcb
leixing <leixing1012@gmail.com>
2017-05-24 16:42:40 +0800

    #json-parser库 添加从String流式解析列表数据的方法

--- json-parser/src/main/java/com/example/JsonParser.java
+++ json-parser/src/main/java/com/example/JsonParser.java
@@ -3,13 +3,15 @@ package com.example;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JsonParser {
-    public <T> List<T> streamParseJson(Class<T> cls, Reader reader, int pageSize, int pageIndex) throws IOException {
+    public <T> List<T> fromJson(Class<T> cls, Reader reader, int pageSize, int pageIndex) throws IOException {
         List<T> list = new ArrayList<>();
         JsonReader jsonReader = new JsonReader(reader);
         Gson gson = new Gson();
@@ -37,5 +39,10 @@ public class JsonParser {
         reader.close();
         jsonReader.close();
         return list;
+    }
+
+    public <T> List<T> fromJson(String json, Class<T> cls, int pageSize, int pageIndex) throws IOException {
+        InputStreamReader reader = new InputStreamReader(new ByteArrayInputStream(json.getBytes()));
+        return fromJson(cls, reader, pageSize, pageIndex);
     }
 }
