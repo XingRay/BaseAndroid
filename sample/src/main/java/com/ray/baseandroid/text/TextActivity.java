db08eaf5fa1e799b0376be14d562d44a76cabd82
leixing <leixing1012@gmail.com>
2017-08-16 14:16:32 +0800

    #update SDK version

--- sample/src/main/java/com/ray/baseandroid/text/TextActivity.java
+++ sample/src/main/java/com/ray/baseandroid/text/TextActivity.java
@@ -1,12 +1,11 @@
 package com.ray.baseandroid.text;
 
-import android.text.method.ArrowKeyMovementMethod;
 import android.text.util.Linkify;
 import android.widget.EditText;
 
 import com.ray.baseandroid.R;
-import com.ray.baseandroid.text.linkstextview.ClickableLinksTextView;
 import com.ray.lib.android.base.page.BaseActivity;
+import com.ray.linkifytext.LinkifyTextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,7 +22,7 @@ import butterknife.OnClick;
 
 public class TextActivity extends BaseActivity {
     @BindView(R.id.tv_text)
-    ClickableLinksTextView tvText;
+    LinkifyTextView tvText;
     @BindView(R.id.et_input)
     EditText etInput;
 
@@ -40,16 +39,17 @@ public class TextActivity extends BaseActivity {
 
     @Override
     protected void loadData() {
-
+        setText();
     }
 
     @OnClick(R.id.bt_click)
     public void onViewClicked() {
-        setText();
+        String input = etInput.getText().toString().trim();
+        tvText.setTextLinkifyAndSelectable(input, Linkify.ALL, true);
     }
 
     private void setText() {
         String text = "hhshxhxhxh 13800090001 hdjxhxh 079183110644 bxjxbxbxj 01013241567 hDjxjzjzj010546764646hxjxjxj 010-15467543 hdjxjxj shhxhdhxHsHxjhxhxhx aaabbb@hecom.cn dhdhjdhxjdhxjdj http://www.baidu.com dhfhhx https://www.baidu.com djxjixhd www.baidu.com hxjxbxhxbbxb www.baidu.com?params=qwe jdjfndjjx hxjbdjx@dhhx.cn dhuxbxb@hdjxh.com.cn djjxhd@163.com jfjcjxnxj";
-
+        tvText.setTextLinkifyAndSelectable(text, Linkify.ALL, true);
     }
 }
