6b41c9799a2f465a2e3bb688af9c0b9803149a09
leixing1012@gmail.com <wsad7206042457>
2018-08-20 14:46:44 +0800

    修复ResUtil的问题，删除与Compat包的依赖

--- android-lib/src/main/java/com/ray/lib/android/util/ResUtil.java
+++ android-lib/src/main/java/com/ray/lib/android/util/ResUtil.java
@@ -1,25 +1,31 @@
 package com.ray.lib.android.util;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.Configuration;
-import android.content.res.Resources;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.v4.content.ContextCompat;
+import android.support.annotation.ColorRes;
+import android.util.TypedValue;
 import android.view.View;
-
-import java.util.Locale;
 
 /**
  * 资源工具类
+ *
+ * @author : leixing
+ * email : leixing1012@qq.com
+ * @date : 2018/7/30 20:09
+ * <p>
  */
+
+@SuppressWarnings("unused")
 public class ResUtil {
 
+    @SuppressLint("StaticFieldLeak")
     private static Context mContext;
-
-    private ResUtil() {
-        throw new UnsupportedOperationException();
-    }
+    private static TypedValue sTempValue;
+    private static final Object LOCK = new Object();
 
     public static void init(Context context) {
         mContext = context;
@@ -65,11 +71,21 @@ public class ResUtil {
      * getColor() 6.0之后过时/弃用问题
      */
     public static int getColor(int resId) {
-        final int version = Build.VERSION.SDK_INT;
-        if (version >= 23) {
-            return ContextCompat.getColor(mContext, resId);
-        } else {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             return mContext.getColor(resId);
+        } else {
+            // noinspection deprecation
+            return mContext.getResources().getColor(resId);
+        }
+    }
+
+    public static ColorStateList getColorStateList(@ColorRes int resId) {
+        final int version = Build.VERSION.SDK_INT;
+        if (version >= Build.VERSION_CODES.M) {
+            return mContext.getResources().getColorStateList(resId, null);
+        } else {
+            // noinspection deprecation
+            return mContext.getResources().getColorStateList(resId);
         }
     }
 
@@ -77,26 +93,47 @@ public class ResUtil {
     /**
      * getDrawable() 5.0之后过时/弃用问题
      */
-    public static final Drawable getDrawable(int resId) {
-        return ContextCompat.getDrawable(mContext, resId);
-    }
-
-    public static String getStringByLocale(Locale locale, int resId) {
-        Resources resources = mContext.getResources();
-        Locale mCurLocale = resources.getConfiguration().locale;
-        Configuration conf = new Configuration(resources.getConfiguration());
-        conf.locale = locale;
-        String result = new Resources(resources.getAssets(), resources.getDisplayMetrics(), conf).getString(resId);
-        conf.locale = mCurLocale;
-        new Resources(resources.getAssets(), resources.getDisplayMetrics(), conf);
-        return result;
+    public static Drawable getDrawable(int resId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return mContext.getDrawable(resId);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            // noinspection deprecation
+            return mContext.getResources().getDrawable(resId);
+        } else {
+            // Prior to JELLY_BEAN, Resources.getDrawable() would not correctly
+            // retrieve the final configuration density when the resource ID
+            // is a reference another Drawable resource. As a workaround, try
+            // to resolve the drawable reference manually.
+            final int resolvedId;
+            synchronized (LOCK) {
+                if (sTempValue == null) {
+                    sTempValue = new TypedValue();
+                }
+                mContext.getResources().getValue(resId, sTempValue, true);
+                resolvedId = sTempValue.resourceId;
+            }
+            // noinspection deprecation
+            return mContext.getResources().getDrawable(resolvedId);
+        }
     }
 
     public static void setBackground(View view, Drawable drawable) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             view.setBackground(drawable);
         } else {
+            // noinspection AliDeprecation
             view.setBackgroundDrawable(drawable);
         }
+    }
+
+    public static int[] getArray(int resId) {
+        TypedArray typedArray = mContext.getResources().obtainTypedArray(resId);
+        int len = typedArray.length();
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = typedArray.getResourceId(i, -1);
+        }
+        typedArray.recycle();
+        return array;
     }
 }
